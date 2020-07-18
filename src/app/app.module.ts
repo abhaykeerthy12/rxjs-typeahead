c0a5bae24549234d5b9254bc49bb2166338eb000
abhaykeerthy12 <abhaykeerthy12@gmail.com>
2020-07-18 21:29:13 +0530

    removed forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {ReactiveFormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +9,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
